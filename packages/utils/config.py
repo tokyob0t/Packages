d9af16fec6ac12f4d81dff25c08605f513126e0d
tokyob0t <tokyob0t.business@proton.me>
2026-03-24 01:25:15 -0300

    chore: update config struct

--- packages/utils/config.py
+++ packages/utils/config.py
@@ -1,8 +1,16 @@
-from dataclasses import dataclass
+from typing import ClassVar, Optional
 
 
-@dataclass(frozen=True, slots=True)
 class Config:
-    VERSION: int
+    VERSION: str
     APPLICATION_ID: str
     SCHEMA_PATH: str
+
+    _instance: ClassVar[Optional["Config"]] = None
+
+    def __new__(cls, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            for k, v in kwargs.items():
+                setattr(cls, k, v)
+        return cls._instance
